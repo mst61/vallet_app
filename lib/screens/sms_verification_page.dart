d6364656ce525df3d0e60960865011465a008f2c
serkanerenn <serkan@yourporter.com>
2021-10-16 10:38:32 +0300

    models and services are added

--- lib/screens/sms_verification_page.dart
+++ lib/screens/sms_verification_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vallet_app/models/verify.dart';
+import 'package:vallet_app/screens/scan_options_page.dart';
 import 'package:vallet_app/services/auth_service.dart';
 
 class SmsVerificationPage extends StatefulWidget {
@@ -79,6 +80,10 @@ class _State extends State<SmsVerificationPage> {
         setState(() {
           apiCall = false;
         });
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ScanOptionsPage()),
+        );
       }, onError: (error) {
         setState(() {
           apiCall = false;
