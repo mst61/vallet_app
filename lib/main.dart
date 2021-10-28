b69ddc40bc34bcc3cf832c3abde4296e262001d1
serkanerenn <serkan@yourporter.com>
2021-10-28 16:03:07 +0300

    marker map

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'DinPro'
+      ),
       home: SplashScreen(),
     );
   }
