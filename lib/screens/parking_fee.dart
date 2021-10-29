c2e2afe82db0421a7cecdd89e5a69e6496da4037
serkanerenn <serkan@yourporter.com>
2021-10-29 13:06:49 +0300

    add licence

--- lib/screens/parking_fee.dart
+++ lib/screens/parking_fee.dart
@@ -4,6 +4,7 @@ import 'package:vallet_app/screens/credit_card_list.dart';
 
 class ParkingFee extends StatefulWidget {
   final Ticket ticket;
+
   const ParkingFee(this.ticket);
 
   @override
@@ -16,151 +17,205 @@ class _State extends State<ParkingFee> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Otopark ücreti'),
-        leading: BackButton(color: Colors.white),
-        centerTitle: true,
-        backgroundColor: Color.fromRGBO(19, 60, 83, 1.0),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios,
+              color: Color.fromRGBO(19, 101, 148, 1.0)),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/loading_gif_blue.png',
+              fit: BoxFit.contain,
+              height: 24,
+            ),
+            Container(
+              padding: const EdgeInsets.all(24.0),
+            )
+          ],
+        ),
+        bottom: PreferredSize(
+            child: Container(
+              child: Divider(color: Color.fromRGBO(19, 101, 148, 1.0)),
+              width: MediaQuery.of(context).size.width * 0.8,
+            ),
+            preferredSize: Size.fromHeight(4.0)),
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+        padding: EdgeInsets.fromLTRB(30, 20, 30, 10),
         child: Column(
           children: <Widget>[
+            Container(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Container()),
             Expanded(
-              child:
-              SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                      decoration: BoxDecoration(
-                        border: Border.fromBorderSide(
-                          BorderSide(
-                              width: 1.0,
-                              color: Color.fromRGBO(185, 207, 221, 1.0)),
-                        ),
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(10.0),
+                child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(60, 0, 60, 0),
+                    decoration: BoxDecoration(
+                      border: Border.fromBorderSide(
+                        BorderSide(
+                            width: 1.0,
+                            color: Color.fromRGBO(19, 101, 148, 1.0)),
                       ),
-                      alignment: Alignment.center,
-                      height: MediaQuery.of(context).size.height * 0.4,
-                      child: new Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          new Container(
-                            padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(Icons.location_city, size: 14),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: Text(
-                                      'İşletme: Parktürk Şişhane',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        fontStyle: FontStyle.normal,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 14.0,
-                                      ),
-                                    ),
-                                  )
-                                ]),
-                          ),
-                          new Container(
-                            padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(Icons.login, size: 14),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: Text(
-                                      'Giriş saati: 12:24',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        fontStyle: FontStyle.normal,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 14.0,
-                                      ),
-                                    ),
-                                  )
-                                ]),
-                          ),
-                          new Container(
-                            padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(Icons.logout, size: 14),
-                                  Padding(
-                                    padding: const EdgeInsets.all(5.0),
-                                    child: Text(
-                                      'Çıkış saati: 13:24',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        fontStyle: FontStyle.normal,
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 14.0,
-                                      ),
-                                    ),
-                                  )
-                                ]),
-                          ),
-                          new Container(
-                            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(Icons.money, size: 25),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      widget.ticket.price.toString() + ' TL',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                          fontStyle: FontStyle.italic,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 25.0),
-                                    ),
-                                  )
-                                ]),
-                          ),
-                        ],
-                      ),
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(3.0),
                     ),
-                  ],
-                ),
-              )
-            ),
+                    alignment: Alignment.center,
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        new Container(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/loading_gif_blue.png',
+                                      fit: BoxFit.contain,
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      'İşletme: ',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Color.fromRGBO(19, 101, 148, 1.0),
+                                        fontSize: 12.0,
+                                      ),
+                                    ),
+                                  ]
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: Text(
+                                    ' Parktürk Şişhane',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(19, 101, 148, 1.0),
+                                      fontSize: 12.0,
+                                    ),
+                                  ),
+                                )
+                              ]),
+                        ),
+                        new Container(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Container(
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.arrow_forward_ios, size: 10, color: Color.fromRGBO(19, 101, 148, 1.0),),
+                                      Text(
+                                        ' Çıkış saati:',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: Color.fromRGBO(19, 101, 148, 1.0),
+                                          fontSize: 12.0,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child: Text(
+                                    ' 13:24',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(19, 101, 148, 1.0),
+                                      fontSize: 12.0,
+                                    ),
+                                  ),
+                                )
+                              ]),
+                        ),
+                        new Container(
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Container(
+                                  padding: const EdgeInsets.all(2.0),
+                                  child:
+                                      Row(
+                                        children: [
+                                          Icon(Icons.arrow_back_ios, size: 10, color: Color.fromRGBO(19, 101, 148, 1.0),),
+                                          Text(
+                                            ' Çıkış saati:',
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              color: Color.fromRGBO(19, 101, 148, 1.0),
+                                              fontSize: 12.0,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                ),
+                                Container(
+                                  child: Text(
+                                    ' 13:24',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(19, 101, 148, 1.0),
+                                      fontSize: 12.0,
+                                    ),
+                                  ),
+                                )
+                              ]),
+                        ),
+                        new Container(
+                          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    widget.ticket.price.toString() + ' TL',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color:
+                                            Color.fromRGBO(19, 101, 148, 1.0),
+                                        fontSize: 32.0),
+                                  ),
+                                )
+                              ]),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )),
             Container(
               padding: EdgeInsets.fromLTRB(0, 0, 0, 15),
               child: Row(
                 mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   ElevatedButton.icon(
                     onPressed: () {
                       Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              CreditCardList()
-                        )
-                      );
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CreditCardList()));
                     },
                     icon: Icon(Icons.payment),
                     label: Text('Ödeme Yöntemi Seç',
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold)),
+                        style: TextStyle(color: Colors.white)),
                     style: ElevatedButton.styleFrom(
                       fixedSize:
-                          new Size(MediaQuery.of(context).size.width - 40, 50),
-                      padding: EdgeInsets.all(15),
-                      side: BorderSide(
-                          width: 2.0,
-                          color: Color.fromRGBO(185, 207, 221, 1.0)),
+                          new Size(MediaQuery.of(context).size.width - 60, 40),
                       primary: Color.fromRGBO(19, 101, 148, 1.0),
                     ),
                   )
